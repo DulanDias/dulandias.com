492322c8f97c7837a76685dd92328ae0ad2cdce5
Dulan Dias <dulan@ieee.org>
2019-10-23 14:20:44 +0530

    added cssl-2019 award

--- index.php
+++ index.php
@@ -1181,6 +1181,21 @@ I have also engaged in a number of extra-curricular skills, with the most recent
                             <!-- ITEMS -->
                             <div class="portfolio-items media-grid masonry" data-layout="masonry" data-item-width="340">
                             
+                                   <!-- item -->
+                                                            
+                                   <div id="awd1" class="media-cell national image hentry">			
+                                    <div class="media-box">
+                                        <img src="images/awards/cssl-2019.jpg" alt="portfolio-post">
+                                        <div class="mask"></div>
+                                    </div>
+        
+                                    <div class="media-cell-desc">
+                                        <h3>CSSL ICT Student of the Year 2019 - Computer Society of Sri Lanka</h3>
+                                    </div>
+        
+                                </div>
+                                <!-- item -->
+
                             <!-- item -->
                                                             
                             <div id="awd1" class="media-cell uni image hentry">			
