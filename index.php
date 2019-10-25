8d1408e402637761b18f77efa003bedc5dccbc35
Dulan Dias <dulan@ieee.org>
2019-10-25 13:50:12 +0530

    added tute-10

--- index.php
+++ index.php
@@ -1434,6 +1434,20 @@ I have also engaged in a number of extra-curricular skills, with the most recent
                             <!-- ITEMS -->
                             <div class="portfolio-items media-grid masonry" data-layout="masonry" data-item-width="320">
                         
+
+                            <div class="media-cell tute file hentry">
+                                    <div class="media-box">
+                                        
+                                        <img src="images/courses/csc110.png" alt="portfolio-post">
+                                        <div class="mask"></div>
+                                        <a href="files/courses/tutorials/csc110/tute-10.pdf" target="_blank"></a>
+                                    </div>
+                                    <div class="media-cell-desc">
+                                        <h3>CSC 110 2.0 Object Oriented Programming - Tute 10</h3>
+                                        <p class="category">October 2019</p>
+                                    </div>
+                                </div>
+
 						 <div class="media-cell tute file hentry">
                                     <div class="media-box">
                                         
